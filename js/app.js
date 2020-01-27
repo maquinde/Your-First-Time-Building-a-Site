1fd77b845ec0622557bd9bf74c0ff95b1beaeb76
Mike <maquinde@outlook.com>
2020-01-27 06:14:50 -0800

    completed course

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var followButton = document.getElementById("follow-button");
+
+followButton.addEventListener("click", function(){
+    var socialMediaDiv = document.getElementById("social-media-section");
+    socialMediaDiv.classList.toggle("active");
+})
